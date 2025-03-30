673c57c61c056c606aa62da38d0fa0a292639a50
TheWExploiter <nuggetsebastian90@gmail.com>
2025-03-30 17:35:06 +0200

    Update SlapFarm.lua

--- SlapFarm.lua
+++ SlapFarm.lua
@@ -1,1 +1,33 @@
+local player = game.Players.LocalPlayer
+local mouse = player:GetMouse()
 
+function teleportToRandomPlayer()
+    local players = game.Players:GetPlayers()
+    local randomPlayer = players[math.random(1, #players)]
+    if randomPlayer.Character and randomPlayer.Character:FindFirstChild("HumanoidRootPart") then
+        local targetPosition = randomPlayer.Character.HumanoidRootPart.Position
+        player.Character:MoveTo(targetPosition)
+    end
+end
+
+function teleportToGuidePlace()
+    local guidePlacePosition = Vector3.new(17892, -130, -3539)
+    player.Character:MoveTo(guidePlacePosition)
+end
+
+function autoSlap()
+    while true do
+        mouse.Button1Down:Fire()
+        wait(0.1)
+    end
+end
+
+spawn(autoSlap)
+
+while true do
+    teleportToRandomPlayer()
+    wait(5)
+    teleportToGuidePlace()
+    wait(10)
+end
+-- Made By Cat
